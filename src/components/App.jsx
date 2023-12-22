690d105a7932203e0a1eaf2cd1cd6d32f3099204
ihorkoi <koleev.igor@gmail.com>
2023-12-22 15:19:53 +0200

    add fonts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
 
 export const App = () => {
-  return <div>
-     <Routes>
-        <Route path="/"/>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
       </Routes>
-  </div>;
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { App } from 'components/App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
