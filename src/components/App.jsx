2d99693ef59eb7b2798ea4e36f33e7e3f850f2e0
Tonya-Pronina <darkpixus@gmail.com>
2023-12-24 00:31:38 +0200

    add WelcomePage + add Container to Common/Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
+import WelcomePage from '../pages/WelcomePage';
 
 export const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<div>Start</div>}/>
+        <Route path="/" element={<div>Start</div>} />
+        <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
     </div>
   );
