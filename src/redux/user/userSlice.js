4974808bb81f047e4775eb9c788ccafd081decc4
vladkasyan <voadkasyan.2006@gmail.com>
2023-12-24 15:12:44 +0200

    start

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  dailyWater,
+  waterNorma,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  extraReducers: builder => {
+    builder
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(logIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      });
+  },
+});
+
+export const authReducer = authSlice.reducer;
