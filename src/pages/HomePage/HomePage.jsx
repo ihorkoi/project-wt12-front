b3bc3de1e352811b20ca118733c680f504864274
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2024-01-07 22:44:28 +0200

    waterRatioPanel first commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,14 @@
 import { TodayWaterList } from 'components/TodayWaterList/TodayWaterList';
 import Container from 'components/common/Container';
 import { Wrapper } from './HomePage.styled';
+import { WaterRatioPanel } from 'components/WaterRatioPanel/WaterRatioPanel';
 
 export default function HomePage() {
   return (
     <Wrapper>
       <Container>
         <TodayWaterList />
+        <WaterRatioPanel />
       </Container>
     </Wrapper>
   );
