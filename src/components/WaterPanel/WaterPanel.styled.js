ea185338bed55354d663252dbb81be4ba1b9596e
ihorkoi <koleev.igor@gmail.com>
2024-01-07 21:20:01 +0200

    Update WaterPanel.styled.js

--- src/components/WaterPanel/WaterPanel.styled.js
+++ src/components/WaterPanel/WaterPanel.styled.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const WaterPanelWrapper = styled.div`
+max-height:840px;
 width:280px;
 margin: 0 auto;
 border-radius:10px;
@@ -8,10 +9,12 @@ padding:24px 8px;
 background-color: var(--secondary-verylightblue);
 box-shadow: 0px 4px 14px 0px rgba(64, 123, 255, 0.30);
 @media screen and (min-width: 768px) {
+    max-height:690px;
     width:704px;
     padding:32px 24px;
   }
 @media screen and (min-width: 1440px) {
+   max-height:680px;
    width:592px;
 }
 `
