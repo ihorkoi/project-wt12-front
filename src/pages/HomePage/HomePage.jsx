8454ba03d8ba383431b27b4199f7528b01b15a5a
ihorkoi <koleev.igor@gmail.com>
2024-01-07 16:20:17 +0200

    add styles on HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
-import { TodayWaterList } from "components/TodayWaterList/TodayWaterList";
+import { TodayWaterList } from 'components/TodayWaterList/TodayWaterList';
 import Container from 'components/common/Container';
+import { Wrapper } from './HomePage.styled';
 
 export default function HomePage() {
-    return(
-        <div>
-            <Container>
-                 <TodayWaterList/>
-            </Container>
-</div>
-    )
+  return (
+    <Wrapper>
+      <Container>
+        <TodayWaterList />
+      </Container>
+    </Wrapper>
+  );
 }
