0ba74a26eca22d26e02078e24a5969fe71a6eb2d
ihorkoi <koleev.igor@gmail.com>
2024-01-07 20:10:09 +0200

    Update waterSlice.js

--- src/redux/water/waterSlice.js
+++ src/redux/water/waterSlice.js
@@ -4,30 +4,7 @@ import { addWaterRecord, editWaterRecord, deletetWaterRecord, getTodayWater } fr
 const initialState = {
     isLoading: false,
     error: null,
-    waterTodayRecords: [
-        // {
-        //     "_id": "659943264af493dcfd6f64aa",
-        //     "waterAmount": "500",
-        //     "time": "14:11",
-        //     "owner": "658c5ef82dee29032d0379c3"
-        // },
-        // {
-        //     "_id": "659946dba6589be166d412f3",
-        //     "waterAmount": "120",
-        //     "time": "12:00",
-        //     "owner": "658c5ef82dee29032d0379c3",
-        //     "createdAt": "2024-01-06T12:26:03.530Z",
-        //     "updatedAt": "2024-01-06T12:26:03.530Z"
-        // },
-        // {
-        //     "_id": "65994dc249bfaac4f669aff2",
-        //     "waterAmount": "720",
-        //     "time": "12:00",
-        //     "owner": "658c5ef82dee29032d0379c3",
-        //     "createdAt": "2021-10-10T00:00:00.000Z",
-        //     "updatedAt": "2024-01-06T12:26:03.530Z"
-        // }
-    ],
+    waterTodayRecords: [],
     waterMonthRecords: [],
 };
 
