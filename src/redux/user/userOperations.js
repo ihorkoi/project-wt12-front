eac950ad4a1285e29b8a2a241b27e25f24508a12
vladkasyan <voadkasyan.2006@gmail.com>
2024-01-07 13:41:28 +0200

    add updateUserInfo

--- src/redux/user/userOperations.js
+++ src/redux/user/userOperations.js
@@ -3,35 +3,46 @@ import axios from 'axios';
 
 axios.defaults.baseURL = '';
 
-export const fetchStorages = createAsyncThunk(
-  'storages/fetchStorages',
-  async (_, thunkAPI) => {
-    try {
-      const response = await axios.get('/storages');
-      return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
-    }
-  }
-);
+// export const fetchStorages = createAsyncThunk(
+//   'storages/fetchStorages',
+//   async (_, thunkAPI) => {
+//     try {
+//       const response = await axios.get('/storages');
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+//   }
+// );
 
-export const addStorage = createAsyncThunk(
-  'storages/addStorage',
-  async ({ count }, thunkApi) => {
-    try {
-      const respone = await axios.post('/storages', { count });
-      return respone.data;
-    } catch (error) {
-      return thunkApi.rejectWithValue(error);
-    }
-  }
-);
+// export const addStorage = createAsyncThunk(
+//   'storages/addStorage',
+//   async ({ count }, thunkApi) => {
+//     try {
+//       const respone = await axios.post('/storages', { count });
+//       return respone.data;
+//     } catch (error) {
+//       return thunkApi.rejectWithValue(error);
+//     }
+//   }
+// );
 
-export const deleteStorage = createAsyncThunk(
-  'storages/deleteStorage',
-  async (id, thunkAPI) => {
+// export const deleteStorage = createAsyncThunk(
+//   'storages/deleteStorage',
+//   async (id, thunkAPI) => {
+//     try {
+//       const response = await axios.delete(`/storages/${id}`);
+//       return response.data;
+//     } catch (e) {
+//       return thunkAPI.rejectWithValue(e.message);
+//     }
+
+//   }
+export const updateUserInfo = createAsyncThunk(
+  'user/updateUserInfo',
+  async (userData, thunkAPI) => {
     try {
-      const response = await axios.delete(`/storages/${id}`);
+      const response = await axios.post(`/storages/updateUserInfo`, userData);
       return response.data;
     } catch (e) {
       return thunkAPI.rejectWithValue(e.message);
--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -1,8 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  dailyWater: [],
-  waterNorma: Number(),
+  avatar: '',
+  name: '',
+  dailyNorma: 0,
   status: 'idle',
   error: null,
 };
@@ -12,43 +13,56 @@ const userSlice = createSlice({
   initialState,
   extraReducers: builder => {
     builder
-      .addCase(fetchStorages.pending, state => {
+      .addCase(updateUserInfoAsync.pending, state => {
         state.status = 'loading';
       })
-      .addCase(fetchStorages.fulfilled, (state, action) => {
+      .addCase(updateUserInfoAsync.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.tasks = action.payload;
-        state.error = null;
+        state.avatar = action.payload.avatar;
+        state.name = action.payload.name;
+        state.dailyNorma = action.payload.dailyNorma;
       })
-      .addCase(fetchStorages.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      })
-      .addCase(addStorage.pending, state => {
-        state.status = 'loading';
-      })
-      .addCase(addStorage.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-        state.tasks.push(action.payload);
-        state.error = null;
-      })
-      .addCase(addStorage.rejected, (state, action) => {
-        state.status = 'failed';
-        state.error = action.error.message;
-      })
-      .addCase(deleteStorage.pending, state => {
-        state.status = 'loading';
-      })
-      .addCase(deleteStorage.fulfilled, (state, action) => {
-        state.tasks = state.items.filter(item => item.id !== action.payload.id);
-        state.status = 'succeeded';
-        state.error = null;
-      })
-      .addCase(deleteStorage.rejected, (state, action) => {
+      .addCase(updateUserInfoAsync.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       });
   },
+  // .addCase(fetchStorages.pending, state => {
+  //   state.status = 'loading';
+  // })
+  // .addCase(fetchStorages.fulfilled, (state, action) => {
+  //   state.status = 'succeeded';
+  //   state.tasks = action.payload;
+  //   state.error = null;
+  // })
+  // .addCase(fetchStorages.rejected, (state, action) => {
+  //   state.status = 'failed';
+  //   state.error = action.error.message;
+  // })
+  // .addCase(addStorage.pending, state => {
+  //   state.status = 'loading';
+  // })
+  // .addCase(addStorage.fulfilled, (state, action) => {
+  //   state.status = 'succeeded';
+  //   state.tasks.push(action.payload);
+  //   state.error = null;
+  // })
+  // .addCase(addStorage.rejected, (state, action) => {
+  //   state.status = 'failed';
+  //   state.error = action.error.message;
+  // })
+  // .addCase(deleteStorage.pending, state => {
+  //   state.status = 'loading';
+  // })
+  // .addCase(deleteStorage.fulfilled, (state, action) => {
+  //   state.tasks = state.items.filter(item => item.id !== action.payload.id);
+  //   state.status = 'succeeded';
+  //   state.error = null;
+  // })
+  // .addCase(deleteStorage.rejected, (state, action) => {
+  //   state.status = 'failed';
+  //   state.error = action.error.message;
+  // });
 });
 
 export const userReducer = userSlice.reducer;
