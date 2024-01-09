7c980126d7dffd2a7c8607bdfeb76d08b7a94a2f
ihorkoi <koleev.igor@gmail.com>
2024-01-09 18:06:34 +0200

    added time on EditModal

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -5,54 +5,51 @@ import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
 import {
-    // Wrapper,
-    Paragrapher,
-    FormWrapper,
-    Form,
-    Label,
-    Input,
-    PasswordContainer,
-    TogglePassword,
-    Img,
-    BTN,
-    NavLinkStyle,
-    ErrorsMess
-} from "./SignUpForm.styled";
-import eyeSlash from "../../img/icons/eye-slash.svg";
-import eye from "../../img/icons/eye.svg";
+  // Wrapper,
+  Paragrapher,
+  FormWrapper,
+  Form,
+  Label,
+  Input,
+  PasswordContainer,
+  TogglePassword,
+  Img,
+  BTN,
+  NavLinkStyle,
+  ErrorsMess,
+} from './SignUpForm.styled';
+import eyeSlash from '../../img/icons/eye-slash.svg';
+import eye from '../../img/icons/eye.svg';
 import Notiflix from 'notiflix';
-
 
 export const registrationSchema = yup
   .object()
-    .shape({
+  .shape({
     email: yup.string().email('Invalid email').required('Email is required'),
     password: yup
       .string()
       .min(8, 'Password must be at least 8 characters long')
-          .required('Password is required'),
-    passwordConfirmation: yup.string()
-     .oneOf([yup.ref('password'), null], 'Passwords must match')
+      .required('Password is required'),
+    passwordConfirmation: yup
+      .string()
+      .oneOf([yup.ref('password'), null], 'Passwords must match'),
   })
   .required();
 
+export function SignUpForm() {
+  const dispatch = useDispatch();
 
-
-export function SignUpForm() {  
-      const dispatch = useDispatch();
-
-      const {
+  const {
     register,
     handleSubmit,
-    formState: { errors,isDirty  },
+    formState: { errors, isDirty },
   } = useForm({
-      defaultValues: { email: '', password: '', passwordConfirmation:'' },
+    defaultValues: { email: '', password: '', passwordConfirmation: '' },
     resolver: yupResolver(registrationSchema),
   });
 
-    const onSubmit = ({ email, password }) => {
-        const name = email;
-    dispatch(registerUser({  email, name, password }))
+  const onSubmit = ({ email, password }) => {
+    dispatch(registerUser({ email, password }))
       .unwrap()
       .then(() => Notiflix.Notify.success('Account successfully created!'))
       .catch(e =>
@@ -63,78 +60,75 @@ export function SignUpForm() {
           : Notiflix.Notify.failure('Something went wrong, try one nore time!')
       );
   };
-    
-    
-    const [showPassword, setShowPassword] = useState(false);
-    const [showConfigPassword, setShowConfigPassword] = useState(false)
 
-        const togglePasswordVisibility = () => {
-            setShowPassword(!showPassword);
-        };
-        const togglePasswordConfirmationVisibility = () => {
-            setShowConfigPassword(!showConfigPassword);
-        };
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfigPassword, setShowConfigPassword] = useState(false);
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const togglePasswordConfirmationVisibility = () => {
+    setShowConfigPassword(!showConfigPassword);
+  };
 
-
-    return (
-        // <Wrapper>
-                <FormWrapper>
-                    <Paragrapher>Sign Up</Paragrapher>
-                    <Form onSubmit={handleSubmit(onSubmit)}>
-                        <Label>Enter your email</Label>
-                    <Input
-                        {...register('email')}
-                        type="text"
-                        id='email'
-                        placeholder='E-mail'
-                        {...(isDirty && !!errors.email ? { haserror: "true" } : {})}
-                    />
-                            <ErrorsMess>{errors.email?.message}</ErrorsMess>
-                        <Label>Enter your password</Label>
-                        <PasswordContainer>
-                        <Input
-                            {...register('password')}
-                            type={showPassword ? 'text' : 'password'}
-                            id="password"
-                            name="password"
-                            placeholder='Password'
-                            {...(isDirty && !!errors.password ? { haserror: "true" } : {})}
-                        />
-                        <TogglePassword onClick={togglePasswordVisibility}>
-                            {showPassword ? (
-                                <Img src={eye} alt="eye-icon" />) :
-                                (<Img src={eyeSlash} alt="eye-slash-icon" />)}
-                        </TogglePassword>
-                                <ErrorsMess>{errors.password?.message}</ErrorsMess>
-                            </PasswordContainer>
-                        <Label>Repeat password</Label>
-                        <PasswordContainer>
-                        <Input
-                            {...register('passwordConfirmation')}
-                            type={showConfigPassword ? 'text' : 'password'}
-                            id='passwordConfirmation'
-                            name='passwordConfirmation'
-                            placeholder='Repeat password'
-                            {...(isDirty && !!errors.passwordConfirmation ? { haserror: "true" } : {})}
-                        />
-                        <TogglePassword onClick={togglePasswordConfirmationVisibility}>
-                            {showConfigPassword ? (
-                                <Img src={eye} alt="eye-icon" />) :
-                                (<Img src={eyeSlash} alt="eye-slash-icon" />)}
-                        </TogglePassword>
-                        <ErrorsMess>{errors.passwordConfirmation?.message}</ErrorsMess>
-                            </PasswordContainer>
-                        <BTN type="submit">Sign Up</BTN>
-                </Form>
-                <NavLinkStyle to="/signin">
-                    Sign In
-                </NavLinkStyle>
-                    </FormWrapper>
-        // </Wrapper>
-    )
-    }
-  
-
-
-
+  return (
+    // <Wrapper>
+    <FormWrapper>
+      <Paragrapher>Sign Up</Paragrapher>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Label>Enter your email</Label>
+        <Input
+          {...register('email')}
+          type="text"
+          id="email"
+          placeholder="E-mail"
+          {...(isDirty && !!errors.email ? { haserror: 'true' } : {})}
+        />
+        <ErrorsMess>{errors.email?.message}</ErrorsMess>
+        <Label>Enter your password</Label>
+        <PasswordContainer>
+          <Input
+            {...register('password')}
+            type={showPassword ? 'text' : 'password'}
+            id="password"
+            name="password"
+            placeholder="Password"
+            {...(isDirty && !!errors.password ? { haserror: 'true' } : {})}
+          />
+          <TogglePassword onClick={togglePasswordVisibility}>
+            {showPassword ? (
+              <Img src={eye} alt="eye-icon" />
+            ) : (
+              <Img src={eyeSlash} alt="eye-slash-icon" />
+            )}
+          </TogglePassword>
+          <ErrorsMess>{errors.password?.message}</ErrorsMess>
+        </PasswordContainer>
+        <Label>Repeat password</Label>
+        <PasswordContainer>
+          <Input
+            {...register('passwordConfirmation')}
+            type={showConfigPassword ? 'text' : 'password'}
+            id="passwordConfirmation"
+            name="passwordConfirmation"
+            placeholder="Repeat password"
+            {...(isDirty && !!errors.passwordConfirmation
+              ? { haserror: 'true' }
+              : {})}
+          />
+          <TogglePassword onClick={togglePasswordConfirmationVisibility}>
+            {showConfigPassword ? (
+              <Img src={eye} alt="eye-icon" />
+            ) : (
+              <Img src={eyeSlash} alt="eye-slash-icon" />
+            )}
+          </TogglePassword>
+          <ErrorsMess>{errors.passwordConfirmation?.message}</ErrorsMess>
+        </PasswordContainer>
+        <BTN type="submit">Sign Up</BTN>
+      </Form>
+      <NavLinkStyle to="/signin">Sign In</NavLinkStyle>
+    </FormWrapper>
+    // </Wrapper>
+  );
+}
